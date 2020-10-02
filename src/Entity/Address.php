c5d7f7fc5696c3d86f42dd06aedeff92ef00ec6f
Chanfi Attoumani <karobwe@netc.fr>
2020-10-02 11:02:37 +0200

    Création des entités

---
+++ src/Entity/Address.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AddressRepository::class)
+ */
+class Address
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $postCode;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Housing::class, mappedBy="address")
+     */
+    private $housings;
+
+    public function __construct()
+    {
+        $this->housings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPostCode(): ?string
+    {
+        return $this->postCode;
+    }
+
+    public function setPostCode(string $postCode): self
+    {
+        $this->postCode = $postCode;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Housing[]
+     */
+    public function getHousings(): Collection
+    {
+        return $this->housings;
+    }
+
+    public function addHousing(Housing $housing): self
+    {
+        if (!$this->housings->contains($housing)) {
+            $this->housings[] = $housing;
+            $housing->setAddress($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHousing(Housing $housing): self
+    {
+        if ($this->housings->contains($housing)) {
+            $this->housings->removeElement($housing);
+            // set the owning side to null (unless already changed)
+            if ($housing->getAddress() === $this) {
+                $housing->setAddress(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getStreet() . ' ' . $this->getPostCode() . ' ' . $this->getCity();
+    }
+}
---
+++ src/Entity/Housing.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HousingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HousingRepository::class)
+ */
+class Housing
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $shortDescription;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $roomCount;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $bedroomCount;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $floorArea;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Address::class, inversedBy="housings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $address;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Type::class, inversedBy="housings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Status::class, inversedBy="housings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    public function setShortDescription(string $shortDescription): self
+    {
+        $this->shortDescription = $shortDescription;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getRoomCount(): ?int
+    {
+        return $this->roomCount;
+    }
+
+    public function setRoomCount(int $roomCount): self
+    {
+        $this->roomCount = $roomCount;
+
+        return $this;
+    }
+
+    public function getBedroomCount(): ?int
+    {
+        return $this->bedroomCount;
+    }
+
+    public function setBedroomCount(int $bedroomCount): self
+    {
+        $this->bedroomCount = $bedroomCount;
+
+        return $this;
+    }
+
+    public function getFloorArea(): ?float
+    {
+        return $this->floorArea;
+    }
+
+    public function setFloorArea(float $floorArea): self
+    {
+        $this->floorArea = $floorArea;
+
+        return $this;
+    }
+
+    public function getAddress(): ?Address
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?Address $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(?Type $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?Status
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?Status $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getType()->getName() . ' ' . 
+            $this->getRoomCount() . ' rooms ' . 
+            $this->getFloorArea() . 'm² ' . 
+            $this->getPrice() . '€ ' . 
+            $this->getAddress()->getCity();
+    }
+}
