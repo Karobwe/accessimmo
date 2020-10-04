079863ee5ffbc8fba5d65511e37a7963f538baa2
Chanfi Attoumanni <attoumani.chanfi@gmail.com>
2020-10-04 08:41:51 +0200

    Ajout des champs des tables étrangère sur Housing

--- src/Controller/Admin/HousingCrudController.php
+++ src/Controller/Admin/HousingCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Housing;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class HousingCrudController extends AbstractCrudController
 {
@@ -12,14 +16,20 @@ class HousingCrudController extends AbstractCrudController
         return Housing::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('shortDescription'),
             TextEditorField::new('description'),
+            IntegerField::new('price'),
+            IntegerField::new('roomCount'),
+            IntegerField::new('bedroomCount'),
+            IntegerField::new('floorArea'),
+            AssociationField::new('address')->autocomplete(),
+            AssociationField::new('type'),
+            AssociationField::new('status'),
+            AssociationField::new('images')
+            // price, roomCount, bedroomCount, floorArea, address, type, status, images
         ];
     }
-    */
 }
